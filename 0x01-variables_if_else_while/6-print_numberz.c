7fad1016c724553238d05874b3d0be05e47883cf
johnolox88 <johnolox88@gmail.com>
2022-06-05 20:02:35 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,8 @@
-/**
-  *
-  *File: 6-print_numberz.c
-  *
-  *Auth: Brennan D Baraban
-  *
-  */
-
 #include <stdio.h>
 
 /**
   *
-  *main - Prints all single digit numbers of base 10 starting from 0,
-  *
-  *only using putchar and without char variables.
+  *main - Prints the lowercase alphabet in reverse.
   *
   *
   *
@@ -24,14 +14,20 @@ int main(void)
 
 {
 
-int num;
+	char letter;
 
-for (num = 0; num < 10; num++)
 
-putchar((num % 10) + '0');
 
-putchar('\n');
+	for (letter = 'z'; letter >= 'a'; letter--)
 
-return (0);
+						putchar(letter);
+
+
+
+	putchar('\n');
+
+
+
+	return (0);
 
 }
