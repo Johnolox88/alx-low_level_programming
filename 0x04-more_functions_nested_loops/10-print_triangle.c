d1b9b7e0a364e89d394a2e04572653d6931f58e8
johnolox88 <johnolox88@gmail.com>
2022-06-10 11:13:28 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_triangle(int size) - prints a triangle followed by a line
+ *
+ * Return: 1 if c is a special character, otherwise return 0
+ *
+ * @c: is a special character
+ *
+ */
+void print_triangle(int size);
+{
+	int i = print_triangle(int size);
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
