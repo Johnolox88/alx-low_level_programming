b742a857d204b858a9546adc5fb61849ef9edd8b
johnolox88 <johnolox88@gmail.com>
2022-06-10 09:34:38 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - multiplies two integers
+ *
+ * Return: 1 if a, b are integers and 0 if otherwise
+ *
+ * @a, b: are integers
+ */
+
+int mul(int a, int b)
+{
+	int i = mul(int a, int b);
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
