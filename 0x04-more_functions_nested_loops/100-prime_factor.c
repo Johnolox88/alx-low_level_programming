5b5367cee590ae1c89fbec402efb5fa638924607
johnolox88 <johnolox88@gmail.com>
2022-06-10 11:50:14 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * getMaxPrimeFactor - finds the maximum prime factor of a number
+ *
+ */
+
+int getMaxPrimeFactor(int n)
+{
+	int i, max = -1;
+	while (n%2 == 0)
+	{
+		max = 2;
+		n = n/2
+	}
+	for ( i = 3; i <= sqrt(n); i=i+2)
+	{
+		while (n % i == 0)
+			
+			max = i;
+			n = n/i
+	}
+}
+if (n > 2)
+{
+	max = n;
+}
+
+return max;
+}
+main () {
+
+	int n;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	printf("Max prime factor: %d", getMaxPrimeFactor(n));
+}
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_number(int n) - prints an integer
+ *
+ * Return: 1 if c is an integer, otherwise return o
+ *
+ * @c: is an integer
+ *
+ */
+
+void print_number(int n)
+{
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+
+}`
