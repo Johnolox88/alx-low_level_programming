cc801aeb8efe033ac72de90124e068c45b1000ae
johnolox88 <johnolox88@gmail.com>
2022-06-10 10:32:13 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype>
+
+/**
+ * print_diagonal(int n) - prints diagonal line at the terminal
+ *
+ * Return: 1 if c is a special character otherwise return 0
+ *
+ * @c: is a special character
+ *
+ */
+void print_diagonal(int n);
+{
+	int i = print_diagonal(int n);
+	
+	if (i > 0)
+	{
+		return (1)
+	}
+	else
+	{
+		return (0);
+	}
+
+}
