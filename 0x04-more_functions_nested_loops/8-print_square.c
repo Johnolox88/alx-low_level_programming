44c546a6fcb9c9fc20cad235f045f1d9aa68c6b7
johnolox88 <johnolox88@gmail.com>
2022-06-10 10:39:13 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_square(int size) - prints a square followed by a line
+ *
+ * Return: 1 if c is a special character, otherwise return 0
+ *
+ * @c: is a special character
+ *
+ */
+void print_square(int size);
+{
+	int i = print_square(int size);
+
+	if ( i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
