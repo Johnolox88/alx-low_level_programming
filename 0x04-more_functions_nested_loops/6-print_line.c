87c280247bc235a860576af290c04a70176d28b5
johnolox88 <johnolox88@gmail.com>
2022-06-10 10:25:47 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_line(int n) - draws a straight line in the terminal
+ *
+ * Return: 1 if c is a _ otherwise return 0
+ *
+ * @c: is a special character
+ *
+ */
+void print_line(int n);
+{
+	int i = print_line(int n);
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
