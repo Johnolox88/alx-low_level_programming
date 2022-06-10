348cd5d866ce7f643b37dd2962b6cb60d7eebb9e
johnolox88 <johnolox88@gmail.com>
2022-06-10 06:18:47 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - funtion that prints upper case
+ * @c: character type alphabet
+ * Return: 1 if uppercase, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	int i = _isupper(c);
+
+	if (i > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
