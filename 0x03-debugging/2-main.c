19cadfc338686f70cccc2d714e754a2955e31254
johnolox88 <johnolox88@gmail.com>
2022-06-10 19:29:42 -0700

    2-print_alphabet.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -1,32 +1,45 @@
+#include <stdio.h>
+
 #include "main.h"
 
+
+
 /**
-  *
-  *main - prints the largest of 3 integers
-  *
-  *Return: 0
-  *
-  */
+ *
+ * * main - prints the largest of 3 integers
+ *
+ * * Return: 0
+ *
+ * */
+
 
 
 int main(void)
 
 {
 
-	     int a, b, c;
+	        int a, b, c;
 
-	     int largest;
+		        int largest;
 
-		a = 972;
 
-		b = -98;
 
-		c = 0;
+			        a = 972;
 
-	     largest = largest_number(a, b, c);
+				        b = -98;
 
-	     printf("%d is the largest number\n", largest);
+					        c = 0;
 
-	     return (0);
+
+
+						        largest = largest_number(a, b, c);
+
+
+
+							        printf("%d is the largest number\n", largest);
+
+
+
+								        return (0);
 
 }
