ac10e7079649e1981f32c7cf9c567b41d9907bd4
johnolox88 <johnolox88@gmail.com>
2022-06-15 01:59:07 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - prints string in reverse
+ * @s: char type
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	
+	while (i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
